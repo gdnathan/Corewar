015422a3d8f68bc5c67a1fc84e8104cdf82e1efe
TheSola10 <thesola10@bobile.fr>
2020-05-23 18:22:35 +0200

    Using WIP source for Corewar

--- corewar/src/main.c
+++ corewar/src/main.c
@@ -1,15 +1,43 @@
 /*
 ** EPITECH PROJECT, 2019
-** CPE_corewar_2019
+** CPE_corewar_2018
 ** File description:
-** main.c
+** main
 */
 
-#ifndef __TESTS
+#include "opcodes.h"
 
-int main(int argc, char **argv)
+op_t opdefs[23] = {
+    { "live",  0x1,  1, { T_DIR } },
+    { "ld",    0x2,  2, { T_DIR, T_REG } },
+    { "st",    0x3,  2, { T_REG, T_REG } },
+    { "st",    0x3,  2, { T_REG, T_IND } },
+    { "add",   0x4,  3, { T_REG, T_REG, T_REG } },
+    { "sub",   0x5,  3, { T_REG, T_REG, T_REG } },
+    { "and",   0x6,  3, { T_REG, T_DIR, T_REG } },
+    { "or",    0x7,  3, { T_REG, T_DIR, T_REG } },
+    { "xor",   0x8,  3, { T_REG, T_DIR, T_REG } },
+    { "zjmp",  0x9,  1, { T_IND } },
+    { "ldi",   0xa,  3, { T_IND, T_IND, T_REG } },
+    { "sti",   0xb,  3, { T_REG, T_IND, T_IND } },
+    { "sti",   0xb,  3, { T_REG, T_IND, T_REG } },
+    { "sti",   0xb,  3, { T_REG, T_REG, T_IND } },
+    { "sti",   0xb,  3, { T_REG, T_REG, T_REG } },
+    { "fork",  0xc,  1, { T_IND } },
+    { "lld",   0xd,  2, { T_DIR, T_REG } },
+    { "lldi",  0xe,  3, { T_REG, T_IND, T_IND } },
+    { "lldi",  0xe,  3, { T_REG, T_IND, T_REG } },
+    { "lldi",  0xe,  3, { T_REG, T_REG, T_IND } },
+    { "lldi",  0xe,  3, { T_REG, T_REG, T_REG } },
+    { "lfork", 0xf,  1, { T_IND } },
+    { "aff",   0x10, 1, { T_REG } }
+};
+
+#ifndef __TESTS
+int main(int ac, char **av)
 {
+    (void)ac;
+    (void)av;
     return (0);
 }
-
 #endif //__TESTS
